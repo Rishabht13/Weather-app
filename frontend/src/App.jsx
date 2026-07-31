e1439ef94399b66bd7ba098118022ee88659cee7
Rishabh Tiwari <pt64632@gmail.com>
2026-07-31 16:58:17 +0530

    feat: add error handling and loading state to weather data fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,32 +5,51 @@ import axios from "axios";
 function App() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async () => {
     if (!city.trim()) {
       return;
     }
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `http://api.weatherapi.com/v1/current.json?key=632a3c2560504418875173031262807&q=${city}`,
+      );
+      
+      setWeatherData(response.data);
+      setError(null);
 
-    const response = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=632a3c2560504418875173031262807&q=${city}`,
-    );
 
-    setWeatherData(response.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setWeatherData(null);
+      setError("Failed to fetch weather data");
+    }finally {
+    setLoading(false);
+    }
     setCity("");
+
   };
 
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <input type="text" placeholder="Enter city name" value={city} onChange={(e) => setCity(e.target.value)}
-      onKeyPress={(e) => {
-        if (e.key === "Enter") {
-          handleSearch();
-        }
-      }}
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
+          }
+        }}
       />
       <button onClick={handleSearch}>Get Weather</button>
       <div className="weather-info">
+        {error && <p className="error">{error}</p>}
         {weatherData && (
           <div>
             <h2>
